ff76095969ac1e856cc1f2977fb05a0b1c9cc257
young03 <wuyang.bnu@139.com>
2019-01-02 21:31:46 +0800

    quality_control

--- bin/1.quality-control.sh
+++ bin/1.quality-control.sh
@@ -1,5 +1,6 @@
-### directories for store scripts and log files
+### prepare dirs and scripts
 mkdir script log
+ln -s ~/users/wuyang/20181218-Cancer-WES-pipe/bin/ .
 
 ### prepare data files
 python bin/output_cmd.py -f ~/data/20181218-TJZLYY-YJP-breastCancer-WES/data_info.txt -d ~/data/20181218-TJZLYY-YJP-breastCancer-WES/ -t rename -o dat >script/rename.sh
@@ -11,6 +12,3 @@ fastqc dat/*/*.fq.gz -o fastqc_output &>log/fastqc.log    # ls */*gz |xargs -I [
 
 ### run multiqc
 multiqc fastqc_output/ -o multiqc_output &>log/multiqc.log
-
-# ps 87986 at dnode14
-
